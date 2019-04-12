bcda439e91ada1cb2597b04c0ec85da133af5813
icw82 <icw82@yandex.ru>
2019-04-12 09:05:41 +0500

    updates

--- storeroom/.emacs.lisp
+++ storeroom/.emacs.lisp
@@ -1,6 +1,5 @@
-(setq make-backup-files nil)
-(setq auto-save-default nil)
-(setq auto-save-list-file-name nil)
+(setq make-backup-files nil) ; stop creating backup~ files
+(setq auto-save-default nil) ; stop creating #autosave# files
 
 (setq display-time-24hr-format t) ;; 24-часовой временной формат в mode-line
 (display-time-mode t) ;; показывать часы в mode-line
@@ -25,4 +24,52 @@
 (setq search-highlight t)
 (setq query-replace-highlight t)
 
-;; alt+x customize-th
+(defun move-line-up ()
+  "Move up the current line."
+  (interactive)
+  (transpose-lines 1)
+  (forward-line -2)
+  (indent-according-to-mode)
+)
+
+
+(defun move-line-down ()
+  "Move down the current line."
+  (interactive)
+  (forward-line 1)
+  (transpose-lines 1)
+  (forward-line -1)
+  (indent-according-to-mode)
+)
+
+
+(global-set-key [(meta up)]  'move-line-up)
+(global-set-key [(meta down)]  'move-line-down)
+
+
+(defun dublicate-line-up ()
+  "Dublicate up the curent line."
+  (interactive)
+  (move-beginning-of-line 1)
+  (kill-line)
+  (yank)
+  (open-line 1)
+  (next-line 1)
+  (yank)
+  (forward-line -1)
+)
+
+
+(defun dublicate-line-down ()
+  "Dublicate down the curent line."
+  (interactive)
+  (move-beginning-of-line 1)
+  (kill-line)
+  (yank)
+  (open-line 1)
+  (next-line 1)
+  (yank)
+)
+
+(global-set-key [(meta shift up)]  'dublicate-line-up)
+(global-set-key [(meta shift down)]  'dublicate-line-down)
