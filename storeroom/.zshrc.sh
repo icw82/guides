0629f24315fac2b7f37ed5275416bb640fed801c
icw82 <icw82@yandex.ru>
2021-02-21 15:16:55 +0500

    update

--- storeroom/.zshrc.sh
+++ storeroom/.zshrc.sh
@@ -1,6 +1,7 @@
 # icw82's .zshrc
 
 export TERM=xterm-256color
+export LANG="en_US.utf8"
 
 autoload -U compinit; compinit
 #autoload colors; colors
@@ -14,11 +15,44 @@ HISTFILE=~/.zhistory
 SAVEHIST=8200
 HISTSIZE=8200
 
+# Treat the '!' character specially during expansion.
+#setopt BANG_HIST
+
+# Write the history file in the ":start:elapsed;command" format.
+setopt EXTENDED_HISTORY
+
+# Write to the history file immediately, not when the shell exits.
 setopt INC_APPEND_HISTORY
+
+# Share history between all sessions.
+setopt SHARE_HISTORY
+
+# Expire duplicate entries first when trimming history.
+setopt HIST_EXPIRE_DUPS_FIRST
+
+# Don't record an entry that was just recorded again.
+setopt HIST_IGNORE_DUPS
+
+# Delete old recorded entry if new entry is a duplicate.
 setopt HIST_IGNORE_ALL_DUPS
+
+# Do not display a line previously found.
+setopt HIST_FIND_NO_DUPS
+
+# Don't record an entry starting with a space.
 setopt HIST_IGNORE_SPACE
+
+# Don't write duplicate entries in the history file.
+#setopt HIST_SAVE_NO_DUPS
+
+# Remove superfluous blanks before recording entry.
 setopt HIST_REDUCE_BLANKS
-setopt HIST_NO_STORE
+
+# Don't execute immediately upon history expansion.
+setopt HIST_VERIFY
+
+# Beep when accessing nonexistent history.
+setopt HIST_BEEP
 
 
 # Синонимы
